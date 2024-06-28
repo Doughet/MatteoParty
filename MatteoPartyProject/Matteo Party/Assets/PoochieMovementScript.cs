cba91e934c2d83546bf59bffbfa2bd0fac3c8a5f
FISHCAT8 <82520225+Doughet@users.noreply.github.com>
2024-06-28 18:06:36 +0200

    boost

--- MatteoPartyProject/Matteo Party/Assets/PoochieMovementScript.cs
+++ MatteoPartyProject/Matteo Party/Assets/PoochieMovementScript.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class PoochieMovementScript : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 1.0f;
+    [SerializeField] bool invesedHorizontalControls = false;
 
 
     private Rigidbody rb;
+
+
+    //FOR THE BOOSTING
+    private Vector3 boostVector;
+    private Vector3 boostDirection;
+    [SerializeField] private float initialBoostPower;
+    private float boostPower;
+    private float boostAccumulated;
+    [SerializeField] private float boostAccumulationSpeed;
+    [SerializeField] private float boostDuration;
+    private float boostCounter;
+
     private Vector3 lastMovementVector;
     private Vector3 newMovementVector;
 
     // Start is called before the first frame update
     void Start()
     {
+        boostCounter = 0;
+        boostAccumulated = initialBoostPower;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -22,13 +37,18 @@ public class PoochieMovementScript : MonoBehaviour
     {
         MovementController();
 
+
+        AccumulateBoost();
+        RealeaseBoost();
+
         //IN THE END APPLY THE COMPUTED MOVEMENT VECTOR;
         Move();
     }
 
     void Move()
     {
-        rb.velocity = newMovementVector;
+        rb.velocity = newMovementVector + boostVector;
+
 
         lastMovementVector = newMovementVector;
         newMovementVector = Vector3.zero;
@@ -36,9 +56,58 @@ public class PoochieMovementScript : MonoBehaviour
 
     void MovementController()
     {
-        float moveX = -Input.GetAxis("Horizontal") * movementSpeed;
+        int inverser = 1;
+        if (invesedHorizontalControls)
+        {
+            inverser = -1;
+        }
+
+        float moveX = -Input.GetAxis("Horizontal") * movementSpeed * inverser;
         float moveZ = Input.GetAxis("Vertical") * movementSpeed;
 
         newMovementVector += new Vector3(moveX, rb.velocity.y, moveZ);
+    }
+
+    void AccumulateBoost()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            boostAccumulated += Time.deltaTime * boostAccumulationSpeed;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            boostPower = boostAccumulated;
+            boostAccumulated = initialBoostPower;
+        }
+    }
+
+    void RealeaseBoost()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Vector3 direction = new Vector3(newMovementVector.x, 0, newMovementVector.z);
+
+            if(direction != Vector3.zero)
+            {
+                boostDirection = direction.normalized;
+            }
+
+            boostVector = boostDirection * boostPower;
+
+            boostCounter = boostDuration;
+        }
+
+        if(boostCounter > 0)
+        {
+            boostVector = boostDirection * boostPower * boostCounter / boostDuration;
+            boostCounter -= Time.deltaTime;
+        }
+
+        if(boostCounter < 0)
+        {
+            boostCounter = 0;
+            boostVector = Vector3.zero;
+        }
+
     }
 }
